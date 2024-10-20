f25b4044722a1ee3d2224bf899412889bba08a7c
Lenka Polcerova <lenka.polcerova@gmail.com>
2024-10-20 22:04:35 +0200

    hotovo

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,17 +1,22 @@
 import { TShirt } from '../components/TShirt';
 import { TShirtSetup } from '../components/TShirtSetup';
+import { useState } from 'react';
 import './style.css';
 
 export const HomePage = () => {
+  const [tshirtType, setTshirtType] = useState('normalShortSleeve');
+  const [tshirtColor, setTshirtColor] = useState('yellow');
+  const [tshirtPrint, setTshirtPrint] = useState('')
+
   return (
     <div className="container">
       <div className="tshirt-panel">
         <div className="tshirt-panel__tshirt">
-          <TShirt type="normalShortSleeve" color="yellow" print="Ahoj světe" />
+          <TShirt type={tshirtType} color={tshirtColor} print={tshirtPrint} />
         </div>
         <div className="tshirt-panel__setup">
           <h2>Možnosti:</h2>
-          <TShirtSetup />
+          <TShirtSetup onType={setTshirtType} onColor={setTshirtColor} onPrint={setTshirtPrint} />
         </div>
       </div>
     </div>
--- src/pages/components/TShirtSetup/index.jsx
+++ src/pages/components/TShirtSetup/index.jsx
@@ -1,10 +1,10 @@
 import './style.css';
 
-export const TShirtSetup = () => {
+export const TShirtSetup = ({ onType, onColor, onPrint }) => {
   return (
     <form className="tshirt-setup">
       <label className="tshirt-setup__label">Typ:</label>
-      <select className="tshirt-setup__input">
+      <select className="tshirt-setup__input" onChange={(e) => {onType(e.target.value)}} >
         <option value="normalShortSleeve">Krátký rukáv</option>
         <option value="tallShortSleeve">Krátký rukáv (prodloužené)</option>
         <option value="normalLongSleeve">Dlouhý rukáv</option>
@@ -14,7 +14,7 @@ export const TShirtSetup = () => {
       </select>
 
       <label className="tshirt-setup__label">Barva:</label>
-      <select className="tshirt-setup__input">
+      <select className="tshirt-setup__input" onChange={(e) => {onColor(e.target.value)}}>
         <option value="yellow">Žlutá</option>
         <option value="red">Červená</option>
         <option value="blue">Modrá</option>
@@ -22,7 +22,7 @@ export const TShirtSetup = () => {
       </select>
     
       <label className="tshirt-setup__label">Potisk:</label>
-      <input className="tshirt-setup__input" type="text" defaultValue="Ahoj světe" />
+      <input className="tshirt-setup__input" type="text" defaultValue="Ahoj světe" onChange={(e) => {onPrint(e.target.value)}} />
     </form>
   );
 };
